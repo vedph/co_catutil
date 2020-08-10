55d04a27a0c1a4fc1c752d4b5348283b9795897f
phylax <dfusi@hotmail.com>
2020-08-10 23:36:09 +0200

    Lemma filter

---
+++ Catutil.Migration.Test/Entries/LemmaFilterTest.cs
@@ -0,0 +1,47 @@
+﻿using Catutil.Migration.Entries;
+using Xunit;
+
+namespace Catutil.Migration.Test.Entries
+{
+    public sealed class LemmaFilterTest
+    {
+        [Fact]
+        public void Apply_Null_Null()
+        {
+            string filtered = LemmaFilter.Apply(null);
+            Assert.Null(filtered);
+        }
+
+        [Fact]
+        public void Apply_Empty_Empty()
+        {
+            string filtered = LemmaFilter.Apply("");
+            Assert.Equal("", filtered);
+        }
+
+        [Theory]
+        [InlineData(" ", "")]
+        [InlineData("  ", "")]
+        [InlineData(" \t ", "")]
+        [InlineData(" abc", "abc")]
+        [InlineData("abc ", "abc")]
+        [InlineData("ab c \td", "ab c d")]
+        public void Apply_Whitespaces_Ok(string input, string expected)
+        {
+            string filtered = LemmaFilter.Apply(input);
+            Assert.Equal(expected, filtered);
+        }
+
+        [Theory]
+        [InlineData(" ", "")]
+        [InlineData("  ", "")]
+        [InlineData(" \t ", "")]
+        [InlineData(" àbc", "abc")]
+        [InlineData("Ábç ", "abc")]
+        public void Apply_Diacritics_Ok(string input, string expected)
+        {
+            string filtered = LemmaFilter.Apply(input);
+            Assert.Equal(expected, filtered);
+        }
+    }
+}
---
+++ Catutil.Migration/Entries/LemmaFilter.cs
@@ -0,0 +1,64 @@
+﻿using Fusi.Text.Unicode;
+using System.Text;
+
+namespace Catutil.Migration.Entries
+{
+    /// <summary>
+    /// Text filter applied to apparatus lemmata and to any other text to be
+    /// compared with lemmata.
+    /// </summary>
+    public static class LemmaFilter
+    {
+        private static readonly UniData _ud = new UniData();
+
+        /// <summary>
+        /// Apply this filter to the specified text, by keeping only
+        /// letters/apostrophe and whitespaces. All the diacritics are removed,
+        /// and uppercase letters are lowercased. Whitespaces are normalized
+        /// and flattened into space, and get trimmed if initial or final.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <returns>The filtered text.</returns>
+        public static string Apply(string text)
+        {
+            if (string.IsNullOrEmpty(text)) return text;
+
+            StringBuilder sb = new StringBuilder();
+            bool prevWS = true;
+
+            foreach (char c in text)
+            {
+                switch (c)
+                {
+                    case '\'':
+                        sb.Append('\'');
+                        prevWS = false;
+                        break;
+                    default:
+                        if (char.IsWhiteSpace(c))
+                        {
+                            if (prevWS) break;
+                            sb.Append(' ');
+                            prevWS = true;
+                            break;
+                        }
+                        if (char.IsLetter(c))
+                        {
+                            char seg = _ud.GetSegment(c, true);
+                            if (seg != 0) sb.Append(char.ToLower(seg));
+                            prevWS = false;
+                            break;
+                        }
+                        prevWS = false;
+                        break;
+                }
+            }
+
+            // right trim
+            if (sb.Length > 0 && sb[sb.Length - 1] == ' ')
+                sb.Remove(sb.Length - 1, 1);
+
+            return sb.ToString();
+        }
+    }
+}

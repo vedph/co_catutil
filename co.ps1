1cc4d3c7d043f23a960a1bb48260249bc79581a0
Myrmex <dfusi@hotmail.com>
2020-10-02 09:18:38 +0200

    Fixed Q009 wit not detected

--- co.ps1
+++ co.ps1
@@ -5,30 +5,30 @@ $CODIR='c:\users\dfusi\desktop\co'
 del $LOG
 Write-Host 'CO CONVERSION'
 Write-Host '(1) IMPORT CO TEXT (DRY)'
-# Invoke-Expression "$CATUTIL import-text $CODIR 1_*.xls catullus -d"
+Invoke-Expression "$CATUTIL import-text $CODIR 1_*.xls catullus -d"
 cmd /c pause
 
 Write-Host '(2) IMPORT CO TEXT'
-# Invoke-Expression "$CATUTIL import-text $CODIR 1_*.xls catullus"
+Invoke-Expression "$CATUTIL import-text $CODIR 1_*.xls catullus"
 cmd /c pause
 
 Write-Host '(3) PARSE-TXT'
-# Invoke-Expression "$CATUTIL parse-txt catullus $CODIR\items\"
+Invoke-Expression "$CATUTIL parse-txt catullus $CODIR\items\"
 cmd /c pause
 
 Write-Host '(4) BUILD BIBLIO LOOKUP'
 Write-Host "$CATUTIL build-biblio ""$CODIR\4_1 Bibliography.xls"" $CODIR"
-# Invoke-Expression "$CATUTIL build-biblio ""$CODIR\4_1 Bibliography.xls"" $CODIR"
+Invoke-Expression "$CATUTIL build-biblio ""$CODIR\4_1 Bibliography.xls"" $CODIR"
 cmd /c pause
 
 Write-Host '(5) PARSE-APP INTO XSLX'
-# Invoke-Expression "$CATUTIL parse-app catullus $CODIR\ProteusDump.json"
+Invoke-Expression "$CATUTIL parse-app catullus $CODIR\ProteusDump.json"
 cmd /c pause
 
 del $LOG
 Write-Host '(6) PARSE-APP INTO JSON'
-# Invoke-Expression "$CATUTIL parse-app catullus $CODIR\ProteusSave.json"
-# copy -Path $LOG -Destination "$CODIR\log-parse-app.txt"
+Invoke-Expression "$CATUTIL parse-app catullus $CODIR\ProteusSave.json"
+copy -Path $LOG -Destination "$CODIR\log-parse-app.txt"
 cmd /c pause
 
 del $LOG
